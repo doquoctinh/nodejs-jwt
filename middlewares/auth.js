c97d08ab477eb2168f472ee608671a84f7c8321f
Tinh Do <31276748+doquoctinh@users.noreply.github.com>
2023-05-26 02:39:00 +0000

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,12 @@
-const createError = require('http-errors');
-const jwt = require('jsonwebtoken');
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
 
-module.exports = (req, res, next) => { 
-  if (!req.get('Authorization')) {
+module.exports = (req, res, next) => {
+  if (!req.get("Authorization")) {
     return next(createError(401));
   }
 
-  const token = req.get('Authorization').split(' ')[1];
+  const token = req.get("Authorization").split(" ")[1];
   const secret = process.env.SECRET;
   try {
     const decode = jwt.verify(token, secret);
